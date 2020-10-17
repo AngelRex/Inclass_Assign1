61595dfbf13238aa86e8395084b3a5b95457bc8c
Angel Rex <angelrex.2019@gmail.com>
2020-10-17 13:31:23 -0400

    Add project files.

---
+++ Inclass_Assign1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inclass_Assign1
+{
+           class Inclass_Assignment
+        {
+            static void Main()
+            {
+
+                int isum = 0, num1;
+                double dsum = 0.0;
+                Console.WriteLine("\nEnter Numbers Seperated by + delimiter");
+                string input = Console.ReadLine();
+                string[] tokens = input.Split('+');
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    if (int.TryParse(tokens[i], out num1))
+                        isum += num1;
+                    else
+                        dsum += double.Parse(tokens[i]);
+                }
+
+                Console.WriteLine("\n\n\tSum of integer values " + isum);
+                Console.WriteLine("\n\n\tSum of decimal values " + dsum);
+                Console.WriteLine("\n\n\tSum of all input " + (isum + dsum));
+            }
+        }
+    }
+
+
