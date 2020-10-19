d9795c3f7c2c12c19cd8efab9dee0e1b94019605
AngelRex <61211123+AngelRex@users.noreply.github.com>
2020-10-19 15:11:12 -0400

    Update Program.cs

--- Inclass_Assign1/Program.cs
+++ Inclass_Assign1/Program.cs
@@ -2,29 +2,39 @@
 
 namespace Inclass_Assign1
 {
-           class Inclass_Assignment
+    class Inclass_Assignment
+    {
+
+        static void Main()
         {
-            static void Main()
+
+            int isum = 0, num1;
+            double dsum = 0.0, num2;
+            Console.WriteLine("\nEnter Numbers Seperated by + delimiter");
+            string input = Console.ReadLine();
+            string[] tokens = input.Split('+');
+            try
             {
 
-                int isum = 0, num1;
-                double dsum = 0.0;
-                Console.WriteLine("\nEnter Numbers Seperated by + delimiter");
-                string input = Console.ReadLine();
-                string[] tokens = input.Split('+');
-                for (int i = 0; i < tokens.Length; i++)
-                {
-                    if (int.TryParse(tokens[i], out num1))
+                foreach (string x in tokens)
+
+                    if (int.TryParse(x, out num1))
                         isum += num1;
+                    else if (double.TryParse(x, out num2))
+                        dsum += num2;
                     else
-                        dsum += double.Parse(tokens[i]);
-                }
+                        throw new FormatException();
+
 
                 Console.WriteLine("\n\n\tSum of integer values " + isum);
-                Console.WriteLine("\n\n\tSum of decimal values " + Math.Round(dsum,2));
-                Console.WriteLine("\n\n\tSum of all input " + Math.Round(isum + dsum,2));
+                Console.WriteLine("\n\n\tSum of decimal values " + Math.Round(dsum, 2));
+                Console.WriteLine("\n\n\tSum of all input " + Math.Round(isum + dsum, 2));
             }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Avoid alphabets and special characters");
+            }
+
         }
     }
-
-
